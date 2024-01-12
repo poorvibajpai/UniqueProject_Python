d367b97babb985c5c7aaa40a5dadcb1c9dbfcfa3
Poorvi Bajpai <150348534+poorvibajpai@users.noreply.github.com>
2024-01-12 20:23:01 +0530

    Update Captcha.py

--- Captcha.py
+++ Captcha.py
@@ -5,4 +5,4 @@ def generate_alpha_numeric_captcha(length=6):
     captcha_text = ''.join(random.choice(char) for _ in range(length))
     return captcha_text
 generate_captcha = generate_alpha_numeric_captcha()
-print(f"Generates captcha text : {generate_captcha}")
+print(f"Generates Captcha Text : {generate_captcha}")
